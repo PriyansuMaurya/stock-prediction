eb87216056279d1e664e39d49d850120ad6ebdc7
Priyanshu Maurya <priyanshu1999pm@gmail.com>
2023-09-27 06:54:22 +0000

    Initial project setup
    This commit adds the initial project skeleton along with essential files for stock price prediction.

---
+++ src/components/model_trainer.py
@@ -0,0 +1,12 @@
+"""
+    Data ingestion is the process of acquiring and integrating information from 
+    different sources inside a repository. In the context of machine learning, 
+    data ingestion refers to the process of 
+    1. collecting, 
+    2. cleaning, and 
+    3. transforming 
+    data from various sources into a format that can be used by machine learning algorithms
+
+"""
+
+
